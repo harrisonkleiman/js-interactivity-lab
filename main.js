a3aa3bd210d7eb1420f6e5cfcb3e541594ff5aa5
Harrison Kleiman <harrikleiman@gmail.com>
2022-03-16 17:24:39 -0400

    core lab completed- final commit

--- main.js
+++ main.js
@@ -3,29 +3,35 @@ console.log("Hello World")
 
 // Step 2
 
-document.querySelector("form").addEventListener("submit", addMovie)
 const message = document.querySelector("#message")
 
-function addMovie(event) {
+const addMovie = (event) => {
   event.preventDefault()
   let inputField = document.querySelector("input")
   let movie = document.createElement("li")
   const movieTitle = document.createElement("span")
+
   movieTitle.textContent = inputField.value
   movieTitle.addEventListener("click", crossOffMovie)
   movie.appendChild(movieTitle)
+
+  document.querySelector("ul").appendChild(movie)
+
   const deleteBtn = document.createElement("button")
   deleteBtn.textContent = "X"
   deleteBtn.addEventListener("click", deleteMovie)
   movie.appendChild(deleteBtn)
 }
 
-function deleteMovie(event) {
-  event.target.parentElement.remove()
-  message.textContent = "Movie Deleted"
+const add = document.querySelector("form")
+add.addEventListener("submit", addMovie)
+
+const deleteMovie = (event) => {
+  event.target.Node.remove()
+  message.textContent = "Movie Deleted!"
 }
 
-function crossOffMovie(event) {
+const crossOffMovie = (event) => {
   event.target.classList.toggle("checked")
   if (event.target.classList.contains("checked") === true) {
     message.textContent = "Movie watched!"
